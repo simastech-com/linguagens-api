e266808e58d9aeaeb7235f7d76fad8564a115b52
William Simas <contato@simastech.com>
2023-03-30 17:03:07 -0300

    Alterações da aula 4

---
+++ src/main/java/br/com/alura/linguagens/api/LinguagemRepository.java
@@ -0,0 +1,10 @@
+package br.com.alura.linguagens.api;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface LinguagemRepository extends MongoRepository<Linguagem,String>
+{
+    List<Linguagem> findByOrderByRanking();
+}
